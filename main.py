e5e6498ba7e776fb4d749a24c442ab44625540fd
Ixtiyorova Zilola <ixtiyorovazilolaxon@gmail.com>
2026-05-29 16:41:44 +0500

    First commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import asyncio
+from os import getenv
+
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+
+TOKEN = "8648167350:AAFpXEMNyhHBY4-ECOgow-3maTL_rHaCa3s"
+
+dp = Dispatcher()
+
+
+# Command handler
+@dp.message(Command("start"))
+async def command_start_handler(message: Message) -> None:
+    await message.answer("Hello! I'm a bot created with aiogram.")
+
+
+# Run the bot
+async def main() -> None:
+    bot = Bot(token=TOKEN)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
+          
